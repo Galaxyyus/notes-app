7624254d3785dff2700f172207764d454af69553
se-lab <se-lab@email.com>
2026-07-24 16:53:33 +0530

    some more changes

--- src/components/CreateArea/CreateArea.jsx
+++ src/components/CreateArea/CreateArea.jsx
@@ -1,5 +1,10 @@
-function CreateArea(props){
-    
+function CreateArea(props) {
+    return (
+        <div>
+            <textarea name="title-input" id="note-title-input" rows='2' placeholder='Note Title'></textarea>
+            <textarea name="text-input" id="note-text-input" placeholder='Note Body'></textarea>
+        </div>
+    );
 }
 
 export default CreateArea;
